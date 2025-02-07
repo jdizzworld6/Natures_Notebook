d59d64ddb04272445c989f15a555b853663d00f4
Jeremy Hansen <jeremy.j.hansen3420@gmail.com>
2025-02-07 15:24:54 -0500

    created_db

---
+++ model/friends_db.php
@@ -0,0 +1,45 @@
+<?php
+// Creating a class to connect to database then querying results
+require_once 'database.php';
+
+class FriendDB {
+    // Query for getting single friend by id
+    // tested
+    public static function getFriendById($id_friends){
+        $db = new Database();
+        $dbConn = $db->getDBConn();
+
+        if ($dbConn){
+            $query = "
+            SELECT f.id_friends, u.id_user, u.first_name, u.last_name, u.profile_image
+            FROM users u
+            Inner JOIN friends f on f.id_user = u.id_user
+            where f.id_friends = $id_friends
+            "; 
+            $result = $dbConn->query($query);
+            return $result->fetch_assoc();
+        } else {
+            return false;
+        }
+    }
+    
+    // Query for getting all of your friends
+    // tested
+    public static function getAllFriends($id_user){
+        $db = new Database();
+        $dbConn = $db->getDBConn();
+
+        if ($dbConn){
+            $query = "
+            SELECT f.id_friends, f.id_user, f.id_your_friends, u.first_name, u.last_name, u.profile_image
+            FROM friends f
+            join
+            users u on f.id_your_friends = u.id_user
+            Where f.id_user = $id_user
+            "; 
+            return $dbConn->query($query);
+        } else {
+            return false;
+        }
+    }
+}
---
+++ view/login_page.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    require_once '../utilities/security.php';
+    
+?>
+
+
+<html>
+
+</html>
